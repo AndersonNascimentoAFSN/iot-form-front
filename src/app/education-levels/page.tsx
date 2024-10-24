dda516776afd4e7c11f782ae334f31f1ccdabcf3
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-10-24 08:41:54 -0300

    feat: add education levels page

---
+++ src/app/education-levels/page.tsx
@@ -0,0 +1,13 @@
+import { fetchEducationLevels } from "@/http";
+
+export default async function EducationLevelsPage() {
+  const educationLevels = await fetchEducationLevels()
+
+  return (
+    <ul>
+      {educationLevels?.data.map(educationLevel => (
+        <li key={educationLevel.id}>{educationLevel.levelName}</li>
+      ))}
+    </ul>
+  )
+}
